bb4421b540de1b7c41630327e5bfa55cb054124b
itzaeon <ae0n0w.c@gmail.com>
2023-01-13 09:35:17 -0800

    add conver_images.py

---
+++ convert_images.py
@@ -0,0 +1,21 @@
+import io
+import os
+import pyheif
+from PIL import Image
+
+
+for root, dirs, files in os.walk(os.getcwd()):
+    for file in files:
+        if file.endswith('.HEIC'):    
+            print(os.path.join(root, file))
+
+            heif_file = pyheif.read(os.path.join(root, file))
+            image = Image.frombytes(
+                heif_file.mode, 
+                heif_file.size, 
+                heif_file.data,
+                "raw",
+                heif_file.mode,
+                heif_file.stride,
+                )
+            image.save(file[:-5] + ".jpg", "JPEG")
